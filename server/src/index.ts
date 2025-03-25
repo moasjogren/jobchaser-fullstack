453e36da089dc7e69766b544e50c358b34111383
moasjogren <moasjgren@gmail.com>
2025-03-25 15:27:29 +0100

    Remove default port assignment and use environment variable for server port

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,5 +19,5 @@ app.use("/", (req, res) => {
   res.send("WELCOME");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
