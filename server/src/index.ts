6925f85907d97e72a517b2cf5db25f5243e83dc5
moasjogren <moasjgren@gmail.com>
2025-03-25 14:29:03 +0100

    Add root route to respond with a message

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
 import jobRoutes from "./routes/jobRoutes";
+import { Request, Response } from "express";
 
 const app = express();
 
@@ -14,6 +15,10 @@ app.use(express.json());
 
 app.use("/users", userRoutes);
 app.use("/jobs", jobRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Railway funkar");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
