00fd8089cd2f94430ee6a1394d258baf8ee3d428
moasjogren <moasjgren@gmail.com>
2025-03-25 14:48:46 +0100

    Update root route response message and change server port to 5000

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,9 +16,9 @@ app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/jobs", jobRoutes);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Railway funkar kanske?");
+app.use("/", (req, res) => {
+  res.send("WELCOME");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
