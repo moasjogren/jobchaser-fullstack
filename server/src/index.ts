31c684ce96d5c804c7a7d1e2dc312a195924356c
moasjogren <moasjgren@gmail.com>
2025-03-25 16:02:56 +0100

    Set default server port

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,5 +19,6 @@ app.use("/", (req, res) => {
   res.send("WELCOME");
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || "4000";
+
+app.listen(process.env.PORT, () => console.log(`Server running on ${PORT}`));
